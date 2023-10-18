b06ce6a62fc2118bda132fa47bd70cda90943f9d
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2023-10-18 20:41:37 +0500

    Add shadcn-ui library with its style scheme and custom colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./assets/index.css";
+import "./assets/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
