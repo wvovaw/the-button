782c8c9ccb0e2c22dbee41888aac1d2a77b5463f
Vladimir Ivakhno <wvovaw0@gmail.com>
2025-09-10 03:24:30 +0500

    feat(web): add project link

--- apps/web/src/pages/HomePage/components/Hero.tsx
+++ apps/web/src/pages/HomePage/components/Hero.tsx
@@ -8,18 +8,20 @@ export function Hero() {
   const { isAuthenticated } = useAuth()
 
   return (
-    <section className="mx-auto flex flex-col justify-center pt-4 font-body sm:px-6 sm:py-12 lg:flex-row lg:justify-between lg:py-24">
+    <section className="font-body mx-auto flex flex-col justify-center pt-4 sm:px-6 sm:py-12 lg:flex-row lg:justify-between lg:py-24">
       <div className="flex flex-col justify-center rounded-sm text-center sm:p-6 lg:max-w-md lg:text-left xl:max-w-lg">
         <h1 className="font-heading text-5xl font-bold sm:text-6xl">
           The <span className="text-primary">Button</span>
         </h1>
-        <p className="mb-8 mt-6 text-lg sm:mb-12">
+        <p className="mt-6 mb-8 text-lg sm:mb-12">
           A clicker game where with each your click the chance of resetting the meter increases by 1 %. Let&apos;s see,
           how many clicks and time it will take to reach at least 50 points?
         </p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:justify-center sm:space-x-4 sm:space-y-0 lg:justify-start">
-          <Button variant="link" className="px-8 py-3 text-lg font-semibold text-accent">
-            I&apos;m just a link
+        <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:justify-center sm:space-y-0 sm:space-x-4 lg:justify-start">
+          <Button variant="link" className="text-accent px-8 py-3 text-lg font-semibold">
+            <a href="https://github.com/wvovaw/the-button" target="_blank" rel="noreferrer noopener">
+              Source Code
+            </a>
           </Button>
           {isAuthenticated() ? (
             <Button variant="default" className="px-8 py-3 text-lg font-semibold" asChild>
@@ -38,7 +40,7 @@ export function Hero() {
           )}
         </div>
       </div>
-      <div className="xl:h-112 2xl:h-128 mt-8 flex h-72 items-center justify-center sm:h-80 sm:p-6 lg:mt-0 lg:h-96">
+      <div className="mt-8 flex h-72 items-center justify-center sm:h-80 sm:p-6 lg:mt-0 lg:h-96 xl:h-112 2xl:h-128">
         <Stats />
       </div>
     </section>
